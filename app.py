816e5dcc270e6d584f52e0c3f0641dc96f0eff49
ushenginnering <70901123+ushenginnering@users.noreply.github.com>
2022-08-26 12:44:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -315,4 +315,4 @@ if __name__ == "__main__":
 #   app.run(host="0.0.0.0", port=8080)
 #   from waitress import serve
 #   serve(app, host="0.0.0.0", port=5000)
-   app.run(debug=True)
+   app.run(host="0.0.0.0", debug=True)
