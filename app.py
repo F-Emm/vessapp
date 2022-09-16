658e9684ec47445e7000cbec4cafb101ac873f7f
ushenginnering <70901123+ushenginnering@users.noreply.github.com>
2022-09-16 18:32:48 +0100

    Update app.py
    UPDATING REDIRECT LINK

--- app.py
+++ app.py
@@ -939,10 +939,9 @@ def output():
 
   print(f"db_cycle = {db_cycle}")
   print(f"output_cycle = {output_cycle}")
-  direct_to = f'https://www.ushvesapp.ushmoney.net?crew_motivation={c1}&vessel_condition={v1}&consignee_throughput={co1}&weather_condition={w1}&tonnage={t1}&delivery={pca1}&output={output_cycle}&terminal=__&cargo_Type={c_tp}'
+  direct_to = f'https://ushvesapp.ushmoney.net/requestberth.php?crew_motivation={c1}&vessel_condition={v1}&consignee_throughput={co1}&weather_condition={w1}&tonnage={t1}&delivery={pca1}&output={output_cycle}&terminal=__&cargo_Type={c_tp}'
 
   return redirect(direct_to)
-
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
